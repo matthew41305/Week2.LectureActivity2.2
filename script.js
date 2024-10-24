f5c50e0386ce3babaeac6b10eb512e6e81b7171e
Matthew Hudson <matthew41305@gmail.com>
2024-10-23 19:41:50 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+let myGreeting = "Hello World!";
+myGreeting = "Wassup Brothaaa"
+console.log(myGreeting);
+
+
---
+++ script2.js
@@ -0,0 +1,22 @@
+//string variable example
+
+let myVariable = "Hello World!";
+console.log(myVariable);
+
+//change the string variable value
+myVariable = "I changed the value of the string variable here"
+console.log(myVariable);
+
+
+// defined constant rate to prevent error
+const pi = 3.14;
+
+// pi = 2;  will throw an error
+
+
+
+let myNumber = 5;
+
+// this will pull the lowest number between the two assigned variables
+Math.min (myNumber, pi);
+console.log(Math.min(pi, 5));
