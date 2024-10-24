debd4e18597f2f418fbd07837309063b82b3b42e
Matthew Hudson <matthew41305@gmail.com>
2024-10-23 19:55:59 -0700

    added myDude and an example of math min while declaring myNumber

--- script2.js
+++ script2.js
@@ -7,6 +7,13 @@ console.log(myVariable);
 myVariable = "I changed the value of the string variable here"
 console.log(myVariable);
 
+// my example
+
+let myDude = "I am 30 years old";
+console.log(myDude);
+
+myDude = "I am now 30,000 years old, my age changed..."
+console.log(myDude);
 
 // defined constant rate to prevent error
 const pi = 3.14;
@@ -20,3 +27,7 @@ let myNumber = 5;
 // this will pull the lowest number between the two assigned variables
 Math.min (myNumber, pi);
 console.log(Math.min(pi, 5));
+
+// gives output of 3.14 as myDude has no value in this scenario
+Math.max (myNumber, myDude);
+console.log(Math.max(pi, 1));
